e88db805f360874d6ee321b53a17c4b2830ac552
alexantg <alexander4565@outlook.com>
2021-10-05 13:59:03 +0200

    Javalin and gradle fixes

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import io.javalin.Javalin;
+
+public class Application{
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create().start(7000);
+        app.config.enableWebjars();
+
+        //views/
+        //api//
+
+    }
+}
