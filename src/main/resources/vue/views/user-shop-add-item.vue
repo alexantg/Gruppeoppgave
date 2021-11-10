de7f099d9b27959b1c4e31feb38aa1b27df4f3fe
alexantg <64732630+alexantg@users.noreply.github.com>
2021-11-10 10:24:55 +0100

    Modified test class

---
+++ src/main/resources/vue/views/user-shop-add-item.vue
@@ -0,0 +1,17 @@
+<template id="add-item">
+  <div>
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+Vue.component("user-shop-add-item",{
+  template:"add-item"
+})
+</script>
+
+<style scoped>
+h1{
+  color: black;
+}
+</style>
